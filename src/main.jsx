ffa53b86a67066d9059d31dfb79f41b9ba9905d3
Ardemis Boghossian <boghossa@gmail.com>
2025-11-20 23:15:48 +0100

    Clean repo and ignore node_modules

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,27 @@
-
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import './index.css'
 import Layout from './components/Layout'
+
 import Home from './pages/Home'
 import Research from './pages/Research'
 import People from './pages/People'
 import Publications from './pages/Publications'
 import News from './pages/News'
-ReactDOM.createRoot(document.getElementById('root')).render( <React.StrictMode> <BrowserRouter> <Layout> <Routes> <Route path="/" element={<Home />} /> <Route path="/research" element={<Research />} /> <Route path="/people" element={<People />} /> <Route path="/publications" element={<Publications />} /> <Route path="/news" element={<News />} /> } /> </Routes> </Layout> </BrowserRouter> </React.StrictMode>
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/research" element={<Research />} />
+          <Route path="/people" element={<People />} />
+          <Route path="/publications" element={<Publications />} />
+          <Route path="/news" element={<News />} />
+        </Routes>
+      </Layout>
+    </BrowserRouter>
+  </React.StrictMode>
 )
